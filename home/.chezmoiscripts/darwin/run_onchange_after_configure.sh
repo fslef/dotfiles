111c24cffbaeafc9d4b77fbfcd7f36475872e5c1
fslef <23463958+fslef@users.noreply.github.com>
2023-10-05 10:02:34 +0200

    Update finder settings

--- home/.chezmoiscripts/darwin/run_onchange_after_configure.sh
+++ home/.chezmoiscripts/darwin/run_onchange_after_configure.sh
@@ -70,8 +70,23 @@ defaults write com.apple.dock wvous-tr-modifier -int 0
 defaults write com.apple.dock wvous-bl-corner -int 0
 defaults write com.apple.dock wvous-bl-modifier -int 0
 
+# Finder settings
 # Display full POSIX path as Finder window title
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+# list view by default
+defaults write com.apple.finder "FXPreferredViewStyle" -string "Nlsv" && killall Finder
+# Keep folders on top
+defaults write com.apple.finder "_FXSortFoldersFirst" -bool "true" && killall Finder
+# Default search scope to current folder
+defaults write com.apple.finder "FXDefaultSearchScope" -string "SCcf" && killall Finder
+# Automatically empty recycle bin after 30 days
+defaults write com.apple.finder "FXRemoveOldTrashItems" -bool "true" && killall Finder
+# disable file extension change warning
+defaults write com.apple.finder "FXEnableExtensionChangeWarning" -bool "false" && killall Finder
+
+# Top Menu Bar
+# clock display settings
+defaults write com.apple.menuextra.clock "DateFormat" -string "\"EEE HH:mm:ss\""
 
 # Disable Resume system-wide
 defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
